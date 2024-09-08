4173104be43aae8b60339f4a12e7e7eda8db673e
Ionut Blanaru <blanaru.ionut@gmail.com>
2024-09-08 14:26:30 +0000

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,7 @@
-document.querySelector(".hamburger").addEventListener("click", toggleMenu)
+const maxWordCount = 30;
+
+
+document.querySelector(".hamburger").addEventListener("click", toggleMenu);
 
 function toggleMenu() {
     const mobileNav = document.getElementById('mobileNav');
@@ -11,8 +14,11 @@ function generateWords() {
     const numberOfWords = document.getElementById('numberOfWords').value || 1;
     const wordType = document.getElementById('wordType').value || 'all';
     const wordSize = document.getElementById('wordSize').value;
+    const resultText = document.getElementById('result');
+    
 
     let wordList;
+    
     switch (wordType.toLowerCase()) {
         case "noun":
             wordList = NOUNS;
@@ -31,15 +37,24 @@ function generateWords() {
     const filteredWords = wordList.filter(word => !wordSize || word.length == wordSize);
 
     if (filteredWords.length === 0) {
-        document.getElementById('result').innerText = "No words found with the specified criteria.";
+        resultText.innerText = "No words found with the specified criteria.";
         return;
     }
-
-    const words = [];
-    while (words.length < numberOfWords) {
+    
+    if (numberOfWords.value === maxWordCount)
+    {
+        resultText.innerText = "Maximimum 30 Words";
+        return;
+    }
+    else{
+        const words = [];
+        while (words.length < numberOfWords) {
         const word = filteredWords[Math.floor(Math.random() * filteredWords.length)];
         words.push(word);
+        document.getElementById('result').innerText = words.join(', ');
     }
 
-    document.getElementById('result').innerText = words.join(', ');
+    
+    }
+    
 }
